bcd25d275c1a3269ba492a978dae9ebd01b7be46
Aayushd18 <deshmukhaayush81@gmail.com>
2022-10-31 18:43:24 +0530

    update threelancer smart contract

--- contract/contracts/Threelance.sol
+++ contract/contracts/Threelance.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.0;
 error NotRegistered();
 
 contract Users {
-    uint private uid = 0 ;
+  uint private uid = 0 ;
 
-    uint private pid = 0 ;
-
-
+  uint private pid = 0 ;
 
 
   struct User {
@@ -21,82 +19,111 @@ contract Users {
     address useradd;
   }
 
+  enum STATUS {
+    NOT_STARTED,
+    ACTIVE,
+    ENDED
+  }
 
-    
-    enum STATUS {
-        NOT_STARTED,
-        ACTIVE ,
-        ENDED
-    }
+  struct Project {
+    uint projectId;
+    uint bountyAmount;
+    uint deadline;
+    string pname;
+    string desc;
+    STATUS condition;
+    address projOwner;
+    mapping(uint => string) submissions;
+    mapping(address => bool) assignedTo;
+  }
 
+  User[] private usersArr; 
+  Project[] private projArr;
 
-   struct Project {
-       uint projectId ;
-       string pname;
-       string desc;
-       STATUS condition;
-       string deadline ;
-       uint bountyAmount ;
-       address projOwner;
+  mapping(address => bool) private hasRegistered; 
+  mapping(address => uint) private addToId;
 
-   }
-    //    address assignedTo;
-   
+  //! Getter Functions
 
-   
+  function getUserByAdd() external view returns (User memory) {
+    return userArr[addToId[msg.sender]];
+  }
 
-    User[] private usersArr; 
-    Project[] private projArr;
-
- //   mapping(uint => uint) private userToProj;
-    mapping(address => bool) private hasRegistered; 
-    mapping(address => uint) private addToId;
-    
-
-    function userReg(string memory _userName, string memory _bio) external {
-        string[] memory acomp ;
-        uint[] memory  mproj; 
-        uint[] memory  appliedproj; 
-        usersArr.push(User(uid , _userName , _bio , acomp , mproj, appliedproj,  msg.sender  )) ;
-        hasRegistered[msg.sender] = true;
-        addToId[msg.sender] = uid; //for getter
-        uid ++;
-    
-    }
-
-    function addProj( string memory _name , string memory _desc , string memory _deadline , uint _amount  ) external {
-    if (!(hasRegistered[msg.sender])){
-        revert NotRegistered();
-    }
-    projArr.push(Project(pid , _name , _desc , STATUS.NOT_STARTED , _deadline , _amount , msg.sender));
-
-    usersArr[addToId[msg.sender]].myProjs.push(pid);
-
-    pid ++ ;
-    }
-
-
-
-    function applytoproj(uint _projId) external { // applybutton for all projects
-        usersArr[addToId[msg.sender]].appliedProjs.push(_projId);
-    }
-
-    function getAppliedProjs() external view returns (uint[] memory){
+  function getAppliedProjs() external view returns (uint[] memory){
     return usersArr[addToId[msg.sender]].appliedProjs ; // filter from allprojs arr in frontend
+  }
+
+  function getAllProjs() external view returns(Project[] memory){
+    return projArr;
+  }
+
+  function getProjById(uint _projId) public view returns(Project memory) {
+    return projArr[_projId];
+  }
+
+  //! Setter functions
+
+  function userReg(string memory _userName, string memory _bio) external {
+    string[] memory acomp ;
+    uint[] memory  mproj; 
+    uint[] memory  appliedproj; 
+    usersArr.push(User(uid , _userName , _bio , acomp , mproj, appliedproj,  msg.sender  )) ;
+    hasRegistered[msg.sender] = true;
+    addToId[msg.sender] = uid; //for getter
+    uid ++;
+
+  }
+
+  function addProj( string memory _name , string memory _desc , uint _deadline , uint _amount  ) external onlyReg {
+    if (!(hasRegistered[msg.sender])){
+      revert NotRegistered();
     }
 
-    function getAllProjs() external view returns(Project[] memory){
-        return projArr;
-    }
+    projArr.push(Project(pid , _amount , _deadline , _name , _desc , STATUS.NOT_STARTED , msg.sender, address(0)));
+    usersArr[addToId[msg.sender]].myProjs.push(pid);
+    pid ++;
+  }
 
-    function getMyProjs() external view returns(uint[] memory){
-        return usersArr[addToId[msg.sender]].myProjs;  //will use filter function in frontend
-    }
+  function applytoproj(uint _projId) external { // applybutton for all projects
+    Project proj = getProjById(_projId);
+
+    require(proj.condition == STATUS.NOT_STARTED, "Project already assigned");
+    require(proj.deadline > block.timestamp, "Deadline reached");
+    
+    proj.condition = STATUS.ACTIVE;
+    proj.assignedTo = msg.sender;
+
+    usersArr[addToId[msg.sender]].appliedProjs.push(_projId);
+  }
+
+  
+
+  //TODO: User can submit their work done to projects assigned
+  function submitProject(uint _projId, string memory _url) external {
+    Project proj - getProjById(_projId);
+
+    require(proj.condition == STATUS.ACTIVE);
+    require(proj.deadline > block.timestamp, "Deadline reached");
+    require(proj.assignedTo[msg.sender] == true, "Project not assigned");
+
+    proj.submissions[addToId[msg.sender]] = _url;
+
+  }
+
+  //TODO: User can edit their submissions before deadline
+  function editPToject(uint _projId, string memory _url) external {
+    Project proj = getProjById(_projId);
+
+    require(proj.submissions[addToId[msg.sender]] != "", "No project submitted yet");
+
+    proj.submissions[addToId[msg.sender]] = _url;
+  }
+
+  //?: User who completed a project will get a nft upon approval of the project owner (thinking)
 
 
 
 }
-
 
 
 // reg => seeallproj => apply to added proj(add + apply) => seeappliedproj => 
